efed247bc3c267e346ae635af4c2ec89c77f5171
Amala TV <121046726+Amalatv@users.noreply.github.com>
2024-12-16 09:43:19 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://luminotech-api.vercel.app/api/:path*',
+        source: '/auth/:path*',
+        destination: 'https://luminotech-api.vercel.app/auth/:path*',
       },
     ]
   },
